7981042427e9f877b4122c85cb7e7711bc2dd9f6
Dominik Guzei <dominik.guzei@gmail.com>
2017-08-09 16:04:50 +0200

    add basic impress page

---
+++ src/pages/impress.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './impress.scss';
+import josoorLogo from '../theme/images/josoor-logo-vertical-colored.svg';
+
+export default () => (
+  <div className={styles.impress}>
+
+    <img src={josoorLogo} className={styles.josoorLogo} />
+
+    <div className={styles.text}>
+
+      <h1>Impress</h1>
+
+      <p>
+        Copyright © 2016 Josoor. All rights reserved.<br/>
+        Josoor – Verein zur Vernetzung von Flüchtlingen und Unterstützungsinitiativen<br/>
+        Wipplingerstraße 20/18, 1010 Wien<br/>
+        +43 1 532 12 43 – <a href="mailto:info@josoor.eu">info@josoor.eu</a><br/>
+        DVR Nr.: 4016208 – ZVR Nr.: 560373607 VAT Nr.: ATU71261323<br/>
+      </p>
+    </div>
+  </div>
+);
