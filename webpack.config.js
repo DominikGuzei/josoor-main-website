f693cbc9ce5ae009b342af71472dd066d4f68d98
Dominik Guzei <dominik.guzei@gmail.com>
2017-10-13 12:23:56 +0200

    try to fix production build config

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
 
 const IS_STATIC = process.env.PHENOMIC_ENV === 'static';
-const IS_PRODUCTION = process.env.CONTEXT === 'production';
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = (config) => ({
-  devtool: !IS_PRODUCTION ? 'cheap-module-eval-source-map' : 'false',
+  devtool: IS_PRODUCTION ? 'false' : 'cheap-module-eval-source-map',
   entry: {
     [config.bundleName]: [
       !IS_STATIC && require.resolve('webpack-hot-middleware/client'),
@@ -63,7 +63,7 @@ module.exports = (config) => ({
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
         CONTEXT: JSON.stringify(process.env.CONTEXT || 'development'),
       },
     }),
