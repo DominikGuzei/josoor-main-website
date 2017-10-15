9c9b517e72301db238aad29c8bb17ea9fd3dfec6
Dominik Guzei <dominik.guzei@gmail.com>
2017-10-15 22:18:46 +0200

    reference file-loader fallback directly

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = (config) => ({
         test: /\.(?:png|jpg|svg)$/,
         loader: require.resolve('url-loader'),
         options: {
+          fallback: require.resolve('file-loader'),
           limit: 50000,
           name: './assets/[name]-[hash].[ext]',
         }
@@ -49,6 +50,7 @@ module.exports = (config) => ({
         test: /\.(?:otf|ttf)$/,
         loader: require.resolve('url-loader'),
         options: {
+          fallback: require.resolve('file-loader'),
           name: './fonts/[name]-[hash].[ext]',
         }
       },
