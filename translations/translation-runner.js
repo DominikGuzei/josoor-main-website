153c24973bd5e58aa1dbac4e9443668784ad8d17
Dominik Guzei <dominik.guzei@gmail.com>
2018-04-27 22:34:11 +0200

    move locale white lists

--- translations/translation-runner.js
+++ translations/translation-runner.js
@@ -3,5 +3,6 @@ const manageTranslations = require('react-intl-translations-manager').default;
 manageTranslations({
   messagesDirectory: 'translations/messages',
   translationsDirectory: 'source/i18n/locales',
-  languages: ['en', 'ar', 'de']
+  languages: ['en', 'ar', 'de'],
+  whitelistsDirectory: 'translations/white-lists',
 });
