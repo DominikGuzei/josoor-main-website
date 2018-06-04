4abfb6e44abacb65aef27824b11292072be6b3ed
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-04 22:50:46 +0200

    fix title link for blog post list

--- source/components/blog/BlogPostHeader.js
+++ source/components/blog/BlogPostHeader.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import styles from './BlogPostHeader.scss';
 import { ROUTES } from '../../routes';
-import LocaleAwareLink from '../shared/LocaleAwareLink';
 import { buildRoute } from '../../utils/routing';
 
 export default class BlogPostHeader extends Component {
 
   render() {
-    const { post, hasTitleLink } = this.props;
+    const { post, hasTitleLink, language } = this.props;
     return (
       <div>
         {hasTitleLink ? (
-          <LocaleAwareLink className={styles.title} to={buildRoute(ROUTES.BLOG.POST, { id: post.id.substring(3) })}>
+          <Link className={styles.title} to={buildRoute(ROUTES.BLOG.POST, { language, id: post.id.substring(3) })}>
             {post.headline}
-          </LocaleAwareLink>
+          </Link>
         ) : (
           <h1 className={styles.title}>{post.headline}</h1>
         )}
