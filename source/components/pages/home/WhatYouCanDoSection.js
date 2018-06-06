c6879da34ee8e67a7111e8ef59d826ac0bc65b35
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-06 21:03:45 +0200

    remove commented code example

--- source/components/pages/home/WhatYouCanDoSection.js
+++ source/components/pages/home/WhatYouCanDoSection.js
@@ -80,13 +80,3 @@ export default class WhatYouCanDoSection extends Component {
     );
   }
 }
-
-/*
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="CBJ9AFPJX8UGN">
-<input type="image" src="https://www.paypalobjects.com/de_DE/AT/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
-<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-</form>
-
- */
