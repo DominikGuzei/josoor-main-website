9a0f77fbddb9343c111f7cf14a968d8ff1d8b704
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-06 20:40:29 +0200

    open new tab for paypal donations

--- source/components/pages/home/WhatYouCanDoSection.js
+++ source/components/pages/home/WhatYouCanDoSection.js
@@ -56,7 +56,7 @@ export default class WhatYouCanDoSection extends Component {
               />
             </div>
 
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
               <input type="hidden" name="cmd" value="_s-xclick" />
               <input type="hidden" name="hosted_button_id" value="CBJ9AFPJX8UGN" />
               <input
