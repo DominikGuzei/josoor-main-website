0581b416e0f672276133b615c80816838c9e4b77
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-07 14:43:39 +0200

    fix style issues with language select

--- source/components/layout/menu/LanguageSelect/LanguageSelect.js
+++ source/components/layout/menu/LanguageSelect/LanguageSelect.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { defineMessages, intlShape } from 'react-intl';
-import { browserHistory } from 'react-router';
 import Select from 'react-polymorph/lib/components/Select';
 import SelectSkin from 'react-polymorph/lib/skins/simple/SelectSkin';
+import { withTheme } from 'react-polymorph/lib/themes/withTheme';
 import { IDENTIFIERS } from 'react-polymorph/lib/themes/API';
 
 const messages = defineMessages({
@@ -13,12 +13,11 @@ const messages = defineMessages({
   },
 });
 
-export default class LanguageSelect extends Component {
+class LanguageSelect extends Component {
 
   static contextTypes = {
     router: PropTypes.object.isRequired,
     intl: intlShape.isRequired,
-    theme: PropTypes.object,
   };
 
   handleLanguageSelection = (value) => {
@@ -30,10 +29,14 @@ export default class LanguageSelect extends Component {
 
   render() {
     const { intl } = this.context;
-    const { selectTheme } = this.props;
+    const { context, selectTheme } = this.props;
+    // Use inherited theme for all but the select component
+    const theme = Object.assign({}, context.theme, {
+      [IDENTIFIERS.SELECT]: selectTheme
+    });
     return (
       <Select
-        themeOverrides={{ [IDENTIFIERS.SELECT]: selectTheme }}
+        theme={theme}
         options={[
           { label: 'English', value: 'en' },
           { label: 'العربية', value: 'ar' },
@@ -46,3 +49,5 @@ export default class LanguageSelect extends Component {
     );
   }
 }
+
+export default withTheme(LanguageSelect);
