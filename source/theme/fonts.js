2f79b757ec7e8424112260ef4e404ecee0189d8f
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-13 14:48:31 +0200

    try font-display swap technique

--- source/theme/fonts.js
+++ source/theme/fonts.js
@@ -1,18 +1,21 @@
 export const latinFonts = (baseUrl) => (`
   @font-face {
     font-family: 'Tisa-Sans-Regular';
+    font-display: swap;
     src: url('${baseUrl}/fonts/tisa-pro/tisa-sans-pro-regular.woff2') format('woff2'),
     url('${baseUrl}/fonts/tisa-pro/tisa-sans-pro-regular.woff') format('woff');
   }
 
   @font-face {
     font-family: 'Tisa-Sans-Light';
+    font-display: swap;
     src: url('${baseUrl}/fonts/tisa-pro/tisa-sans-pro-light.woff2') format('woff2'),
     url('${baseUrl}/fonts/tisa-pro/tisa-sans-pro-light.woff') format('woff');
   }
 
   @font-face {
     font-family: 'Tisa-Sans-Bold';
+    font-display: swap;
     src: url('${baseUrl}/fonts/tisa-pro/tisa-sans-pro-bold.woff2') format('woff2'),
     url('${baseUrl}/fonts/tisa-pro/tisa-sans-pro-bold.woff') format('woff');
   }
