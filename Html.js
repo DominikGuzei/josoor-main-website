66c815e598001d2429a3004ee50181bf89038889
Dominik Guzei <dominik.guzei@gmail.com>
2018-06-13 16:12:50 +0200

    inline css into html

--- Html.js
+++ Html.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Head from "react-helmet";
 import { omit } from 'lodash';
+import environment from './source/environment';
 
 export default ({ App, render }) => {
   // if needed, you can know if you are in development or in static rendering
   // const isDev = process.env.PHENOMIC_ENV === "development"
-  const { Main, State, Script, Style } = render(<App />);
+  const { Main, State, Script, Style, assets } = render(<App />);
   const helmet = Head.renderStatic();
   return (
     <html {...helmet.htmlAttributes.toComponent()}>
@@ -13,7 +14,25 @@ export default ({ App, render }) => {
         {helmet.meta.toComponent()}
         {helmet.title.toComponent()}
         {helmet.base.toComponent()}
-        <Style />
+        {environment.isStaticBuild() ? (
+          <style
+            // @idea use https://github.com/addyosmani/critical
+            dangerouslySetInnerHTML={{
+              __html: Object.keys(assets)
+                .reduce((acc, name) => acc.concat(assets[name]), [])
+                .filter(asset => asset.endsWith(".css"))
+                .map(css =>
+                  // @todo try to get /dist/ from config somehow
+                  require("fs").readFileSync("./dist/" + css, { encoding: "utf8" })
+                )
+                .join("")
+                .replace(/\n/, "")
+            }}
+          />
+        ) : (
+          <Style />
+        )}
+
         {helmet.link.toComponent()}
         {helmet.style.toComponent()}
         {helmet.script.toComponent()}
